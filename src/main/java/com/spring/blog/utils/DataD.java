91c56c838f0a84d37084845fd4b8aa8f2556d925
Junior-Antonio <antonio.silvajunior.as@gmail.com>
2022-03-23 23:27:25 -0300

    Atualizando texto

--- src/main/java/com/spring/blog/utils/DataD.java
+++ src/main/java/com/spring/blog/utils/DataD.java
@@ -25,13 +25,13 @@ public class DataD {
             post1.setAutor("Pedro Resende");
             post1.setData(LocalDate.now());
             post1.setTitulo("Tecnologia");
-            post1.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+            post1.setTexto("Um monte de coisa escrita aqui!!");
 
             Post post2 = new Post();
             post2.setAutor("Antonio Junior");
             post2.setData(LocalDate.now());
             post2.setTitulo("Tecnologia");
-            post2.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+            post2.setTexto("Um monte de coisa escrita aqui!!");
 
             postList.add(post1);
             postList.add(post2);
